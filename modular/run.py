8e5b03dabf667d5c57d4e9b6cd98093f6910fda3
oddish <bettani@biologie.ens.fr>
2021-10-12 15:00:22 +0200

    catpure works

--- modular/run.py
+++ modular/run.py
@@ -2,28 +2,34 @@ import TIS
 import cv2
 import json
 from collections import deque
-from time import time
+import time
 import logging
-# TODO: put the log in the right place 
-logging.basicConfig(filename='run.log', level=logging.DEBUG)
+# TODO: put the log in the right place
 
-class Properties:
+class Configs:
     def __init__(self, path):
         self.path = path
-        self.properties = {}
+        self.configs = {}
         self.read_configs()
 
     def read_configs(self):
         with open(self.path) as json_file:
-            self.properties = json.load(json_file)
+            self.configs = json.load(json_file)
             json_file.close()
 
 
+a = Configs("configs.json")
+b = Camera(a.configs)
+b.start_capture()
+time.sleep(10)
+b.stop_capture()
+b.queue.loop()
+b.queue.videos[-1].release()
 
 class Camera(TIS.TIS):
-    def __init__(self, properties, timeout_delay=1, expected_frames=0, path_to_output='.'):
+    def __init__(self, configs, timeout_delay=1, expected_frames=0, path_to_output='.'):
         super().__init__()
-        self.properties = properties
+        self.configs = configs
         self.timeout_delay = timeout_delay
         self.expected_frames = expected_frames
         self.path_to_output = path_to_output
@@ -31,60 +37,70 @@ class Camera(TIS.TIS):
         self.queue = None
 
     def open_device(self):
-        self.openDevice(self.properties['serial'],
-                        self.properties['width'],
-                        self.properties['height'],
-                        self.properties['framerate'],
-                        TIS.SinkFormats.fromString(self.properties['serial']),
+        self.openDevice(self.configs['serial'],
+                        self.configs['width'],
+                        self.configs['height'],
+                        self.configs['framerate'],
+                        TIS.SinkFormats.fromString(self.configs['serial']),
                         False)
 
 
     def apply_properties(self):
-        for p in self.properties['properties']:
+        for p in self.configs['properties']:
             self.Set_Property(p['property'], p['value'])
 
 
     def create_callback(self):
-        self.queue = Queue(self.path_to_output, self.timeout_delay, self.expected_frames)
+        self.queue = Queue(self.configs, self.path_to_output, self.timeout_delay, self.expected_frames)
         self.Set_Image_Callback(self.queue.add_frame) # may throw error
-        self.createPipeline(video_path=self.callback.path_to_video) # this as well
 
     def start_capture(self):
         self.open_device()
-        self.apply_properties()
         self.create_callback()
-        # self.startPipeline(self)
+        self.Start_pipeline()
+        self.apply_properties()
+        # self.queue.loop()
         # self.pipeline.set_state(Gst.State.PLAYING) # TODO?
 
     def stop_capture(self):
-        self.stopPipeline()
+        self.Stop_pipeline()
 
 
 class Queue:
-    def __init__(self, path_to_output, expected_frames):
+    def __init__(self, configs, path_to_output, timeout_delay, expected_frames):
+        self.configs = configs
         self.path_to_output = path_to_output
+        self.timeout_delay = timeout_delay
         self.expected_frames = expected_frames
         self.frames = deque()
         self.timestamps = list() #TODO: write them 
         self.counter = 0
         self.videos = []
-        self.time_of_last_frame = time() - self.timeout_delay
+        self.time_of_last_frame = time.time() - self.timeout_delay
+        logging.basicConfig(filename=path_to_output + '/run.log', level=logging.DEBUG)
 
     def loop(self):
-        while True:
-            if time() - self.time_of_last_frame > self.timeout_delay:
+        # while True:
+        for _ in range(300):
+            print("Checking for frames")
+
+            if time.time() - self.time_of_last_frame > self.timeout_delay:
                 self.new_video()
+                print("New video")
 
             if len(self.frames) > 0:
                 self.videos[-1].write(self.frames.popleft())
-                self.time_of_last_frame = time()
+                self.time_of_last_frame = time.time()
+                print("Frame sent to video")
             else:
-                sleep(1e-6)
+                time.sleep(1e-6)
+                print("Sleeping")
                 
 
     def add_frame(self, camera):
-        self.frame.append(camera.Get_image())
-        self.timestamps.append(time())
+        self.frames.append(camera.Get_image())
+        self.timestamps.append(time.time())
+        print("Adding frame")
         self.counter += 1
 
     def new_video(self):
@@ -92,8 +108,8 @@ class Queue:
         if len(self.videos) > 0:
             self.videos[-1].release
 
-
-        self.videos.append(Video(name)) # TODO
+        # TODO: fix framerate
+        self.videos.append(Video(name, 30, self.configs["width"], self.configs["height"]))
 
     def new_video_name(self):
         expected_frames = self.expected_frames*len(self.videos)
@@ -109,7 +125,7 @@ class Queue:
 
         
 class Video(cv2.VideoWriter):
-    def __init__(self, path_to_video, fps, width, height, fourcc_str="XVID"):
+    def __init__(self, path_to_video, fps, width, height, fourcc_str="MJPG"):
         self.fourcc = cv2.VideoWriter_fourcc(*fourcc_str)
         self.path_to_video = path_to_video
         super().__init__(self.path_to_video, self.fourcc, fps, (width, height))
