221cb7acfbf77b68cf79fd8147190146f67ee74b
Romain Fayat <r.fayat@gmail.com>
2021-11-10 10:24:48 +0100

    Improve argument parsing.

--- record.py
+++ record.py
@@ -1,3 +1,4 @@
+"Start a recording from a json parameters file to an output folder."
 import argparse
 from pathlib import Path
 from camera import Camera
@@ -5,17 +6,19 @@ from camera import Camera
 
 parser = argparse.ArgumentParser(__doc__)
 parser.add_argument("config_path",
-                    help="Path to the state file of the camera")
+                    help="Path to the state file of the camera",
+                    type=lambda x: Path(x).expanduser())
 parser.add_argument("-o", "--output",
                     help="Path to the output video folder.",
-                    dest="path_video_folder", default="./videos")
+                    dest="path_video_folder", default=Path("./videos"),
+                    type=lambda x: Path(x).expanduser())
 parser.add_argument("-t", "--test",
                     help="Start in test mode",
                     dest="test_mode", action="store_true")
 
 args = parser.parse_args()
-config_path = str(args.config_path)
-path_video_folder = str(Path(args.path_video_folder).absolute())
+config_path = args.config_path.absolute()
+path_video_folder = args.path_video_folder.absolute()
 test_mode = args.test_mode
 
 if test_mode:
