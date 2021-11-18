6acc63ebe5797df4f7d70f8955283a83782f7127
Romain FAYAT <r.fayat@gmail.com>
2021-11-18 21:19:13 +0100

    run_all working

--- run_all.sh
+++ run_all.sh
@@ -5,15 +5,12 @@ stty -echoctl # hide ^C
 
 # function called by trap
 kill_recordings() {
-	printf "\rSIGINT caught\n"
-	kill -SIGINT $PID0
-	kill -SIGINT $PID1
-	kill -SIGINT $PID2
-	kill -SIGINT $PID3
-	kill -SIGINT $PID4
+	printf "sigint caught\n"
+	for pid in $(cat .pids); do
+		kill -SIGINT $pid
+	done
 	exit
 }
-
 
 trap 'kill_recordings' SIGINT
 
@@ -24,6 +21,9 @@ PID1=$!
 python record.py -c configs.json -o ~/data/cam3 -f -i 2 &
 PID2=$!
 python record.py -c configs.json -o ~/data/cam4 -f -i 3 &
-PID4=$!
+PID3=$!
 python record.py -c configs.json -o ~/data/cam5 -f -i 4 &
-PID5=$!
+PID4=$!
+
+echo -e "$PID0\n$PID1\n$PID2\n$PID3\n$PID4" > .pids
+wait $PID0
