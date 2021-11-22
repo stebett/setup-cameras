d4770a9548c0c95b055acf1505a210142ae7a3ac
stebett <stebett@gmail.com>
2021-11-22 19:23:38 +0100

    added clean command to setup, rewritten readme

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 "Run cameras from TIScamera"
 import os
-from setuptools import setup
+from setuptools import setup, Command
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
 
 
 def read(fname):
@@ -8,18 +18,20 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name="tiscam",
-      version="0.1",
-      author="Stefano Bettani",
-      author_email="s.bettani@tuta.io",
-      description=__doc__,
-      install_requires=["gobject",
-                        "PyGObject",
-                        "PyQt5",
-                        "matplotlib",
-                        "toml",
-                        "vext",
-                        "vext.gi"],
-      packages=["tiscam"],
-      long_description=read('README.md'),
-      )
+        version="0.1",
+        author="Stefano Bettani",
+        author_email="s.bettani@tuta.io",
+        description=__doc__,
+        install_requires=["gobject",
+            "PyGObject",
+            "PyQt5",
+            "matplotlib",
+            "toml",
+            "vext",
+            "vext.gi"],
+        packages=["tiscam"],
+        long_description=read('README.md'),
+        cmdclass={ 'clean': CleanCommand,
+            }
+        )
 
